1fd6db0341414b9b0366139842491eff3122aab6
gpablo6 <gpablo6@outlook.com>
2022-10-31 16:59:05 -0600

    Update endpoints with queries

--- server/main.go
+++ server/main.go
@@ -3,54 +3,92 @@ package main
 import (
 	"bytes"
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"time"
+
+	enders "github.com/5HeadGames/web3-metadata-api/endersapi"
 )
 
-func makeReqest(encodedQuery *bytes.Buffer, apiKey string) *http.Request {
+func buildReqest(encodedQuery *bytes.Buffer, graphApi string) *http.Request {
 	toRequest, _ := http.NewRequest(
 		"POST",
-		apiKey,
+		graphApi,
 		encodedQuery)
 	toRequest.Header.Set("Content-Type", "application/json")
 	return toRequest
 }
 
 func main() {
-	// apiKey := enders.GetEnvVars("SUBGRAPH_API")
+	apiKey := enders.GetEnvVars("SUBGRAPH_API")
 
 	http.HandleFunc("/", func(rw http.ResponseWriter, _ *http.Request) {
 
 		rw.Write([]byte("Enders Subgraph API"))
-
-		// client := &http.Client{Timeout: time.Second * 5}
-		// response, err := client.Do(newRequest)
-		// if err != nil {
-		// 	fmt.Printf("Error processing request %v", err)
-		// }
-
-		// responseData, err := ioutil.ReadAll(response.Body)
-
-		// if err != nil {
-		// 	fmt.Printf("Error reading data response %v", err)
-		// }
-
-		// fmt.Println(string(responseData))
-	})
-
-	http.HandleFunc("/trades", func(rw http.ResponseWriter, req *http.Request) {
-		address := req.URL.Query().Get("buyerAddress")
-		rw.Write([]byte(fmt.Sprintf("Address, %s", address)))
 	})
 
 	http.HandleFunc("/packs", func(rw http.ResponseWriter, req *http.Request) {
-		contract := req.URL.Query().Get("contractAddress")
-		rw.Write([]byte(fmt.Sprintf("Contract Pack, %s", contract)))
+		address := req.URL.Query().Get("contractAddress")
+		toQuery := enders.PacksQuery(address)
+		formattedRequest := buildReqest(enders.EncodeQuery(toQuery), apiKey)
+
+		client := &http.Client{Timeout: time.Second * 5}
+		response, err := client.Do(formattedRequest)
+
+		if err != nil {
+			fmt.Printf("Error processing request %v", err)
+		}
+
+		responseData, err := io.ReadAll(response.Body)
+
+		if err != nil {
+			fmt.Printf("Error reading data response %v", err)
+		}
+
+		rw.Write(responseData)
 	})
 
 	http.HandleFunc("/sales", func(rw http.ResponseWriter, req *http.Request) {
-		address := req.URL.Query().Get("userAddress")
-		rw.Write([]byte(fmt.Sprintf("Address, %s", address)))
+		address := req.URL.Query().Get("sellerAddress")
+		toQuery := enders.SalesQuery(address)
+		formattedRequest := buildReqest(enders.EncodeQuery(toQuery), apiKey)
+
+		client := &http.Client{Timeout: time.Second * 5}
+		response, err := client.Do(formattedRequest)
+
+		if err != nil {
+			fmt.Printf("Error processing request %v", err)
+		}
+
+		responseData, err := io.ReadAll(response.Body)
+
+		if err != nil {
+			fmt.Printf("Error reading data response %v", err)
+		}
+
+		rw.Write(responseData)
+	})
+
+	http.HandleFunc("/buys", func(rw http.ResponseWriter, req *http.Request) {
+		address := req.URL.Query().Get("buyerAddress")
+		toQuery := enders.BuysQuery(address)
+		formattedRequest := buildReqest(enders.EncodeQuery(toQuery), apiKey)
+
+		client := &http.Client{Timeout: time.Second * 5}
+		response, err := client.Do(formattedRequest)
+
+		if err != nil {
+			fmt.Printf("Error processing request %v", err)
+		}
+
+		responseData, err := io.ReadAll(response.Body)
+
+		if err != nil {
+			fmt.Printf("Error reading data response %v", err)
+		}
+
+		rw.Write(responseData)
 	})
 
 	fmt.Println("Server listening on port 8080")
